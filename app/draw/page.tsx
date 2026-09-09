42d94eccdfa3984c2b1fc55c67269ac05b200405
Taeyoung Park <dhfak@naver.com>
2026-09-09 15:47:09 +0900

    feat: add home and draw pages

---
+++ app/draw/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function DrawPage() {
+  return (
+    <main>
+      <Link href="/">홈으로 돌아가기</Link>
+
+      <h1>그림 이어 그리기</h1>
+
+      <p>Canvas 그림판이 들어갈 페이지입니다.</p>
+    </main>
+  );
+}
